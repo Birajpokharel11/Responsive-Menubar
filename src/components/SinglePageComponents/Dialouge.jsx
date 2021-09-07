ad57a69c6b27c6cf2b0896568f429c6c31d6d5b1
Biraj Pokharel <54663280+Birajpokharel11@users.noreply.github.com>
2021-09-07 16:54:30 +0545

    Dialouge completed

--- src/components/SinglePageComponents/Dialouge.jsx
+++ src/components/SinglePageComponents/Dialouge.jsx
@@ -16,6 +16,7 @@ import Vector from './SpecialOfferPic/Vector.svg';
 
 import { Box, Grid, Paper, Typography } from '@material-ui/core';
 import { Container } from 'react-bootstrap';
+import { StylesContext } from '@material-ui/styles';
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -91,6 +92,62 @@ const useStyles = makeStyles((theme) => ({
     fontSize: '18px',
     lineHeight: '21.6px',
     color: '#2A398D'
+  },
+  LineThroughText: {
+    textAlign: 'center',
+    textDecorationLine: 'line-through',
+    color: '#2A398D;'
+  },
+  Discount: {
+    fontFamily: 'lato',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '32px',
+    lineHeight: '38px',
+    color: ' #2A398D',
+    textAlign: 'right'
+  },
+  Cancellation: {
+    fontFamily: 'lato',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '12px',
+    lineHeight: '38px',
+    textAlign: 'center',
+    color: ' #2A398D'
+  },
+  Alldetails: {
+    fontFamily: 'lato',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '16px',
+    lineHeight: '19px',
+    color: '#AB3996',
+    textAlign: 'right'
+  },
+  Questions: {
+    fontFamily: 'lato',
+    fontStyle: 'normal',
+    fontWeight: '300',
+    fontSize: '16px',
+    lineHeight: '19px',
+    color: '#2A398D'
+  },
+  Answers: {
+    fontFamily: 'lato',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '18px',
+    lineHeight: '22px',
+    color: '#2A398D'
+  },
+  DownloadBtn: {
+    fontFamily: 'lato',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: '18px',
+    lineHeight: '22px',
+    color: '#AB3996'
   }
 }));
 
@@ -98,7 +155,7 @@ export default function MaxWidthDialog() {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
   const [fullWidth, setFullWidth] = React.useState(true);
-  const [maxWidth, setMaxWidth] = React.useState('sm');
+  const [text, setText] = React.useState(false);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -107,7 +164,9 @@ export default function MaxWidthDialog() {
   const handleClose = () => {
     setOpen(false);
   };
-
+  const handleTextopen = () => {
+    setText((prev) => !prev);
+  };
   return (
     <React.Fragment>
       <Button
@@ -116,11 +175,13 @@ export default function MaxWidthDialog() {
           maxHeight: '52px',
           backgroundColor: '  #AB3996'
         }}
+        data-cy="ViewOffer"
         onClick={handleClickOpen}
       >
         <Typography className={classes.ButtonTypo}>View Offer</Typography>
       </Button>
       <Dialog
+        style={{ height: '112vh' }}
         fullWidth={fullWidth}
         maxWidth="md"
         open={open}
@@ -167,17 +228,12 @@ export default function MaxWidthDialog() {
                 </Grid>
                 <div style={{ flexGrow: '1' }} />
                 <Grid item md>
-                  <Typography
-                    style={{
-                      textAlign: 'center',
-                      textDecorationLine: 'line-through'
-                    }}
-                  >
-                    € 155.000{' '}
+                  <Typography className={classes.LineThroughText}>
+                    € 155.000
                   </Typography>
                   <br />
-                  <Typography style={{ textAlign: 'center' }}>
-                    € 155.000{' '}
+                  <Typography className={classes.Discount}>
+                    € 155.000
                   </Typography>
                   <br />
                   <Button
@@ -186,6 +242,7 @@ export default function MaxWidthDialog() {
                       maxHeight: '52px',
                       backgroundColor: '  #AB3996'
                     }}
+                    data-cy="Claim Now"
                     onClick={handleClickOpen}
                   >
                     <Typography className={classes.ButtonTypo}>
@@ -194,14 +251,71 @@ export default function MaxWidthDialog() {
                   </Button>
                   <br />
                   <br />
-
-                  <Typography style={{ textAlign: 'center' }}>
+                  <Typography className={classes.Cancellation}>
                     Free cancellation
                   </Typography>
                 </Grid>
               </Grid>
             </Paper>
           </Box>
+          {text && (
+            <Box
+              style={{
+                marginLeft: '4%',
+                marginRight: '4%',
+                marginTop: '2%',
+                marginBottom: '2%'
+              }}
+            >
+              <div style={{ display: 'flex', gap: '3rem' }}>
+                <Typography className={classes.Questions}>
+                  Sample Itinerary
+                </Typography>
+                <Typography className={classes.DownloadBtn}>
+                  Download
+                </Typography>
+              </div>
+              <Box pt={1}>
+                <Typography className={classes.Answers}>
+                  <span className={classes.Questions}>
+                    How can I go to check-in destination?
+                  </span>
+                  <br />
+                  You can go there via Blabla Airport
+                </Typography>
+              </Box>
+              <Box pt={2}>
+                <Typography className={classes.Answers}>
+                  <span className={classes.Questions}>What is included?</span>
+                  <br />
+                  Wages and food for the crew, Croation 13% of VAI, fuel for
+                  main engines up to 4 hours cruising per day, fuel for 24
+                  hours/day of generator use, complete linen and towels, yacht
+                  insurance, service on board, sojourn taxes, yacht cleaning,
+                  Wi-Fi.
+                </Typography>
+              </Box>
+            </Box>
+          )}
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingBottom: '1%'
+            }}
+          >
+            <Button
+              onClick={handleTextopen}
+              data-cy="more-Details-less-Details"
+            >
+              <Box>
+                <Typography className={classes.Alldetails}>
+                  Show {text ? 'Less' : 'More'} Details About This Offer
+                </Typography>
+              </Box>
+            </Button>
+          </div>
         </DialogContent>
         <DialogActions></DialogActions>
       </Dialog>
